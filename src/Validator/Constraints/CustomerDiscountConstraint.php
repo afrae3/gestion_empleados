01167e4e8451f679e5333c02bc6009f0f8e537ce
Afrae <afrae.chakkaf@gmail.com>
2025-11-07 10:30:33 +0100

    Update validator translation and adjust composer dependencies

--- src/Validator/Constraints/CustomerDiscountConstraint.php
+++ src/Validator/Constraints/CustomerDiscountConstraint.php
@@ -7,7 +7,7 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute] // <- Esto es lo que faltaba
 class CustomerDiscountConstraint extends Constraint
 {
-    public string $message = 'El descuento no puede ser mayor al 50%.';
+    public string $message = 'customer_discount.percentage.too_high';
 
     public function validatedBy(): string
     {
